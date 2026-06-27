c3103facfffcbef1265be051ec41f749a82b9277
parul3520 <parulkathiyal1234@gmail.com>
2026-06-27 11:11:30 +0530

    Delete SI.js

--- SI.js
+++
@@ -1,9 +0,0 @@
-function calc(){
-    var num=parseInt(document.getElementById("num").value);
-    var rate=parseInt(document.getElementById("rate").value);
-    var time=parseInt(document.getElementById("time").value);
-     
- var cal=(num*rate*time)/100
-document.getElementById("intro").innerHTML="SI is "+cal;
-
-}
